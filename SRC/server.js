6e12b181bb8ac16fe26f14a3bfa4297d74aa5644
JIZ1998 <jameskzzk@gmail.com>
2022-05-18 19:16:10 +0200

    Basic server Code

---
+++ SRC/server.js
@@ -0,0 +1,17 @@
+// To open server go to your browser and type in "localhost:3000"
+
+const http = require('http')
+const server = http.createServer((req,res)=>{
+   // console.log('request made')
+   console.log(req.url,req.method)
+   //res.setHeader('Content-Type','test/plain');
+   res.setHeader('Content-Type','text/html');
+   res.write('<p>This is the server will use for testing stuff great, testing 123</p>');
+   res.end(); 
+});
+//server is listening on port 3000
+server.listen(3000,'Localhost',()=>{
+    console.log('lsitening for request on port 3000')
+})
+
+
