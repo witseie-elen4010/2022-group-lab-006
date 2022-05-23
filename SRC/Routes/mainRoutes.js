ee6dd8e3cd9729e4f7e803c2b444cc06663b472f
JIZ1998 <jameskzzk@gmail.com>
2022-05-23 17:34:30 +0200

    Updating mainRouter and Web Pages

--- SRC/Routes/mainRoutes.js
+++ SRC/Routes/mainRoutes.js
@@ -3,7 +3,34 @@
 const path = require("path");
 const express = require("express");
 const mainRouter = express.Router();
+
 mainRouter.get("/singlePlayer", function (req, res) {
   res.sendFile(path.join(__dirname, "..", "Views", "singlePlayer.html"));
 });
+
+// Route for the login Page 
+mainRouter.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "Views", "login.html"));
+});
+
+// Route for the Registration Page
+mainRouter.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "Views", "register.html"));
+});
+
+// Route for the Home Page
+mainRouter.get("/home", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "Views", "home.html"));
+});
+
+// Route for the Update Page
+mainRouter.get("/update", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "Views", "update.html"));
+});
+
+// Route for the Multiplayer Page
+mainRouter.get("/multiplayer", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "Views", "multiPlayer.html"));
+});
+
 module.exports = mainRouter;
