4c6d8c13690c59d9374a8557abd8f944862743bf
Stallon Matindike <2234219@students.wits.ac.za>
2022-06-03 17:06:58 +0200

    game IDs made random, randomness may be removed if there is no need

--- SRC/Public/Scripts/Hosting.js
+++ SRC/Public/Scripts/Hosting.js
@@ -6,11 +6,11 @@ document.getElementById("myButton").onclick = function () {
 };
 
 const button = document.getElementById("idButton");
-
+const gameID = Math.floor(Math.random() * 10000000) + 1000000;
 button.addEventListener(
   "click",
   function () {
-    const gameID = 9875690;
+    //const gameID = 9875690;
     document.getElementById("gameIdOutput").textContent = gameID;
   },
   false
