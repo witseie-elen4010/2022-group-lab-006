6fa3bb8ad7893233da00ef091a87dc71097f49bb
Stallon Matindike <2234219@students.wits.ac.za>
2022-06-03 12:56:49 +0200

    basic game id creation

--- SRC/Public/Scripts/Hosting.js
+++ SRC/Public/Scripts/Hosting.js
@@ -1,6 +1,11 @@
-'use strict';
+"use strict";
 
-document.getElementById('myButton').onclick = function () {
-  const numOfPlayers = document.getElementById('Players').value
-  console.log(numOfPlayers, 'players')
+document.getElementById("myButton").onclick = function () {
+  const numOfPlayers = document.getElementById("Players").value;
+  console.log(numOfPlayers, "players");
+};
+
+document.getElementById("idButton").onclick = function () {
+  const gameID = 1845601;
+  document.getElementById("gameIdOutput").innerHTML = gameID;
 };
