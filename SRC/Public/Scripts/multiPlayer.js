f7552cbdd746d9cd2c47b323ed64afc712b294d9
Stallon Matindike <2234219@students.wits.ac.za>
2022-06-03 17:29:06 +0200

    request game id when the user click join in multoplayer mode

---
+++ SRC/Public/Scripts/multiPlayer.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("idButton");
+
+button.addEventListener(
+  "click",
+  function () {
+    let game_id = window.prompt("Please Enter Game ID");
+  },
+  false
+);
