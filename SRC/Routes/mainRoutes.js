74cc92e2fec3482387b19e436addaff28eb9f636
Mhlengeni Miya <363729@gmail.com>
2022-06-07 13:26:12 +0200

    Adding aboutPage to the home page, adding route

--- SRC/Routes/mainRoutes.js
+++ SRC/Routes/mainRoutes.js
@@ -42,6 +42,11 @@ mainRouter.get("/home", function (req, res) {
   res.sendFile(path.join(__dirname, "..", "Views", "home.html"));
 });
 
+// Route for the About Page
+mainRouter.get("/aboutPage", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "Views", "aboutPage.html"));
+});
+
 // Route for the Update Page
 mainRouter.get("/update", function (req, res) {
 
